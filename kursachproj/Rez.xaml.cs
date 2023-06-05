3506bbc1b9c6a01798498d11ac9e218f7aebf727
lwite23 <lwite23@mail.ru>
2023-06-05 12:38:30 +0300

    day16

--- kursachproj/Rez.xaml.cs
+++ kursachproj/Rez.xaml.cs
@@ -23,6 +23,73 @@ namespace kursachproj
         public Rez()
         {
             InitializeComponent();
+            var currentCars = qweEntities1.GetContext().rez.ToList();
+
+            DGridRez.ItemsSource = currentCars;
+
+
+        }
+
+        private void Updaterez()
+        {
+            var currentCars = qweEntities1.GetContext().rez.ToList();
+
+            currentCars = currentCars.Where(p => p.fullname.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            DGridRez.ItemsSource = currentCars;
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            manage.MaimFrame.Navigate(new edit(null));
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            manage.MaimFrame.Navigate(new edit((sender as Button).DataContext as rez));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            manage.MaimFrame.Navigate(new edit(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var productForRemoving = DGridRez.SelectedItems.Cast<rez>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {productForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    qweEntities1.GetContext().rez.RemoveRange(productForRemoving);
+                    qweEntities1.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+
+                    DGridRez.ItemsSource = qweEntities1.GetContext().rez.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                qweEntities1.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridRez.ItemsSource = qweEntities1.GetContext().rez.ToList();
+            }
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Updaterez();
         }
     }
 }
--- kursachproj/edit.xaml.cs
+++ kursachproj/edit.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Data.SqlClient;
+using System.Drawing;
+
+using System.Data;
 
 namespace kursachproj
 {
@@ -20,8 +26,9 @@ namespace kursachproj
     /// </summary>
     public partial class edit : Page
     {
-        private qwe _currentqwe = new qwe();
-        public edit()
+        byte[] imageData;
+        private rez _currentqwe = new rez();
+        public edit(rez selectedqwe)
         {
             InitializeComponent();
 
@@ -30,42 +37,62 @@ namespace kursachproj
 
             DataContext = _currentqwe;
         }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentqwe.fullname))
+                errors.AppendLine("Укажите ФИО.");
+           
+            if (string.IsNullOrWhiteSpace(_currentqwe.education))
+                errors.AppendLine("Укажите образование.");
+            if (string.IsNullOrWhiteSpace(_currentqwe.experience))
+                errors.AppendLine("Укажите опыт работы.");
+            if (string.IsNullOrWhiteSpace(_currentqwe.post))
+                errors.AppendLine("Укажите должность.");
+            if (string.IsNullOrWhiteSpace(_currentqwe.citizenship))
+                errors.AppendLine("Укажите гражданство.");
+            if (string.IsNullOrWhiteSpace(_currentqwe.typeofemployment))
+                errors.AppendLine("Укажите тип занятости.");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentqwe.id == 0)
+                qweEntities1.GetContext().rez.Add(_currentqwe);
+
+            try
+            {
+                qweEntities1.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+        private void MouseLeftButtonUp_Click(object sender, MouseButtonEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image (*.png, *.jpg, *.jpeg) |*.png; *.jpg; *.jpeg";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imageData = File.ReadAllBytes(openFileDialog.FileName);
+                PhotoService.Source = new ImageSourceConverter()
+                    .ConvertFrom(imageData) as ImageSource;
+                _currentqwe.photo = imageData;
+            }
+
+
+        }
+        private void BtnAddPhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
-    private void BtnSave_Click(object sender, RoutedEventArgs e)
-    {
-        StringBuilder errors = new StringBuilder();
-
-        if (string.IsNullOrWhiteSpace(_currentqwe.fullname))
-            errors.AppendLine("Укажите ФИО.");
-        if (string.IsNullOrWhiteSpace(_currentqwe.yearOfBirth))
-            errors.AppendLine("Укажите год рождения.);
-        if (string.IsNullOrWhiteSpace(_currentqwe.education))
-            errors.AppendLine("Укажите образование.");
-        if (_currentqwe.experience == 0)
-            errors.AppendLine("Укажите опыт работы.");
-        if (_currentqwe.post == 0)
-            errors.AppendLine("Укажите должность.");
-        if (string.IsNullOrWhiteSpace(_currentqwe.citizenship))
-            errors.AppendLine("Укажите гражданство.");
-        if (string.IsNullOrWhiteSpace(_currentqwe.typeofemployment))
-            errors.AppendLine("Укажите тип занятости.");
-
-        if (errors.Length > 0)
-        {
-            MessageBox.Show(errors.ToString());
-            return;
-        }
-        if (_currentqwe.Id == 0)
-            qweEntities.GetContext().qwe.Add(_currentqwe);
-
-        try
-        {
-            qweEntities.GetContext().SaveChanges();
-            MessageBox.Show("Информация сохранена");
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(ex.Message.ToString());
-        }
-    }
+    
 }
